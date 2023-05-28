85ed2a0520c95bc38aa8e125a2800b575ebde238
KovalMaxim1989 <110034072+KovalMaxim1989@users.noreply.github.com>
2023-05-28 11:12:01 +0300

    return
    return

--- middlewares/authenticate.js
+++ middlewares/authenticate.js
@@ -17,7 +17,7 @@ const authenticate = async (req, res, next) => {
     const { id } = jwt.verify(token, SECRET_KEY);
     const user = await User.findById(id);
     if (!user) {
-      next(new HttpError(401, "Not authorized"));
+      return next(new HttpError(401, "Not authorized"));
     }
     req.user = user;
     next();
